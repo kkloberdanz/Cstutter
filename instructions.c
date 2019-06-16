c8f1234c28677220196e0173546c9408646ecec1
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2019-06-16 07:51:18 -0500

    instruction operations moved to its own file

---
+++ instructions.c
@@ -0,0 +1,77 @@
+/*
+ * Author: Kyle Kloberdanz
+ * Project Start Date: 27 Nov 2018
+ * License: GNU GPLv3 (see LICENSE.txt)
+ *     This file is part of stutter.
+ *
+ *     stutter is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     stutter is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with stutter.  If not, see <https://www.gnu.org/licenses/>.
+ * File: instructions.c
+ */
+
+#include "instructions.h"
+
+const char *inst_names[] = {
+    "NOP",
+    "PUSH",
+    "ADD",
+    "SUB",
+    "MUL",
+    "DIV",
+    "MOD",
+    "PRINTI",
+    "READI",
+    "PRINTC",
+    "READC",
+    "POP",
+    "LOAD",
+    "STORE",
+    "J",
+    "JZ",
+    "JLEZ",
+    "JNZ",
+    "CALL",
+    "RET",
+    "POPC",
+    "HALT",
+    NULL
+};
+
+bool requires_immediate(inst_t inst) {
+    switch (inst) {
+        case PUSH:
+        case STORE:
+        case LOAD:
+        case J:
+        case JZ:
+        case JNZ:
+        case JLEZ:
+        case CALL:
+            return true;
+        default:
+            return false;
+    }
+}
+
+bool is_jump(inst_t inst) {
+    switch (inst) {
+        case J:
+        case JZ:
+        case JNZ:
+        case JLEZ:
+        case CALL:
+            return true;
+        default:
+            return false;
+    }
+}
